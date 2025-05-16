a5268fd05cc9f5d178abaa9f433a2d91d551dbb7
Daniel Salwasser <daniel.salwasser@outlook.com>
2025-05-16 16:54:38 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = builtins.attrValues {
+          inherit (pkgs) gradle jdk24 gcc14;
+          inherit (pkgs) pre-commit google-java-format;
+        };
+      };
+    }
+  );
+}
